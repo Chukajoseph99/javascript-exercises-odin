0660e4d4473f78ba93ba15dc977d39f0f4f49e72
chukajoseph99 <chukajoseph99@gmail.com>
2023-12-17 16:15:31 +0100

    Javascript exercises to test problem solving

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+function reverseString(string){
+    let array = string.split("");
+    let myArray = [];
+    let i;
+// console.log(array);
+    for(i = 0; i < string.length; i++){
+        myArray.unshift(array[i]);
+    }  
+        return(myArray.join(""));
 
-};
+}
 
 // Do not edit below this line
 module.exports = reverseString;
+// console.log(reverseString("Hello"))
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
+function removeFromArray(arr, ...theArgs) {
+  for (let i = 0; i < theArgs.length; i++) {
+    let arrIndex = arr.indexOf(theArgs[i]);
+    if (arr.includes(theArgs[i])) {
+      arr.splice(arrIndex, 1);
+    }
+  }
+  return arr;
 };
-
+console.log(removeFromArray([1, 2, 3, 4], 3, 2));
 // Do not edit below this line
 module.exports = removeFromArray;
